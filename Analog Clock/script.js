9c6864446b446cccfd0e811c71b495008b916ddf
Kamana54855 <114852248+Kamana54855@users.noreply.github.com>
2022-10-04 18:53:38 +0530

    Add files via upload

---
+++ Analog Clock/script.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+    date = new Date();
+    htime = date.getHours();
+    mtime = date.getMinutes();
+    stime = date.getSeconds();
+
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
